3d6e2986c7fc9f4e25643b42e28c61ae8605d1bb
Bojan <bojan-stanimirovski097@hotmail.com>
2023-04-12 21:05:33 +0200

    added & styled SearchExercises Component

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  return (
+    <>
+      <Stack alignItems="center" justifyContent="center" p="20px" mt="37px">
+        <Typography
+          fontWeight="700"
+          sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+          mb="50px"
+          textAlign="center"
+        >
+          Awesome Exercises You Can Try
+        </Typography>
+
+        <Box position="relative" mb="72px">
+          <TextField
+            height="76px"
+            value=""
+            onChange={(e) => {}}
+            placeholder="Search Exercises"
+            type="text"
+            sx={{
+              input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+              width: { lg: "800px", xs: "350px" },
+              backgroundColor: "#ffffff",
+              borderRadius: "40px",
+            }}
+          ></TextField>
+
+          <Button
+            className="search-btn"
+            sx={{
+              backgroundColor: "#ff2625",
+              color: "#fff",
+              textTransform: "none",
+              width: { lg: "175px", xs: "80px" },
+              fontSize: { lg: "20px", xs: "14px" },
+              height: "56px",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            Search
+          </Button>
+        </Box>
+      </Stack>
+    </>
+  );
 };
 
 export default SearchExercises;
