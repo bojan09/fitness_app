35b7e23987cc416989f07b553ff88a9f6554c1f4
Bojan <bojan-stanimirovski097@hotmail.com>
2023-04-12 20:24:43 +0200

    added main components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+// mui components
+import { Box } from "@mui/material";
+
+// components
+import { HeroBanner, SearchExercises, Exercises } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+      </Box>
+    </>
+  );
 };
 
 export default Home;
